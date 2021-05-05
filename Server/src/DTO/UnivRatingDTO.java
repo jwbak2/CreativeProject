519a9e11c37a8e4eb7e47ef8911d666c4684c532
hyunkim <han.kim@kakao.com>
2021-05-05 17:33:59 +0900

    서버 모듈 만들고 DTO추가

---
+++ Server/src/DTO/UnivRatingDTO.java
@@ -0,0 +1,50 @@
+package DTO;
+
+
+public class UnivRatingDTO {
+    private String userEmail;
+    private String univId;
+    private String content;
+    private Long score;
+    private java.sql.Date creationDate;
+
+    public String getUserEmail() {
+        return this.userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUnivId() {
+        return this.univId;
+    }
+
+    public void setUnivId(String univId) {
+        this.univId = univId;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getScore() {
+        return this.score;
+    }
+
+    public void setScore(Long score) {
+        this.score = score;
+    }
+
+    public java.sql.Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setCreationDate(java.sql.Date creationDate) {
+        this.creationDate = creationDate;
+    }
+}
