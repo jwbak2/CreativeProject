7eedf7a6de016ac9ad31789ef7f9f74517c289f1
jinoo7099 <vxc7099@gmail.com>
2021-05-06 12:02:04 +0900

    [Add] : Client

---
+++ controller/Home.java
@@ -0,0 +1,36 @@
+package client.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+public class Home {
+
+    @FXML
+    private BorderPane bp;
+
+    @FXML
+    private AnchorPane ap;
+
+    @FXML
+    void home(MouseEvent event) {
+        bp.setCenter(ap);
+    }
+
+    @FXML
+    void univDetail(MouseEvent event) {
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("../view/univDetail.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        bp.setCenter(root);
+    }
+
+}
