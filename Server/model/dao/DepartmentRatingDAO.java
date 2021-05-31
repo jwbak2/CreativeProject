7bc5af2b3ce9e150b7b203e9c1e4bc3258515c1e
jwbak <jwbak2@gmail.com>
2021-05-31 10:44:28 +0900

    [fix] : 학과 리스트 조회 완성 - 속도 개선 필요
    대학, 학과 평가 등록 기능 개발중
    클라이언트 종료시 서버 무한루프 오류 수정중

---
+++ Server/model/dao/DepartmentRatingDAO.java
@@ -0,0 +1,11 @@
+package Server.model.dao;
+
+import Server.model.dto.DepartmentRatingDTO;
+
+public class DepartmentRatingDAO {
+
+	public boolean insertDepartmentRating(DepartmentRatingDTO rating) {
+		return true;
+	}
+
+}
