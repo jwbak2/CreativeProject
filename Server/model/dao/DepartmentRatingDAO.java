e462054ab10b5eb33e88b06a649aaa73d4468e56
hyunkim <han.kim@kakao.com>
2021-06-01 15:38:35 +0900

    [Add] : 학과 평가 DAO Insert  +DAO 소소한 코드 수정

--- Server/model/dao/DepartmentRatingDAO.java
+++ Server/model/dao/DepartmentRatingDAO.java
@@ -1,11 +1,48 @@
 package Server.model.dao;
 
+import Server.model.DBCP;
 import Server.model.dto.DepartmentRatingDTO;
+import Server.model.dto.UnivRatingDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DepartmentRatingDAO {
 
+	// 대학 평가 삽입
 	public boolean insert(DepartmentRatingDTO rating) {
-		return true;
+
+		boolean isSucc = false;
+
+		Connection conn = DBCP.getConnection();
+
+		String SQL = "INSERT INTO crtvp.\"user_email\", \"department_id\", \"content\", \"score\", \"creation_date\" VALUES(?,?,?,?)";
+
+		try (PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+
+			pstmt.setString(1, rating.getUserEmail());
+			pstmt.setString(2, rating.getDepartmentId());
+			pstmt.setString(3, rating.getContent());
+			pstmt.setLong(4, rating.getScore());
+			pstmt.setDate(5,  rating.getCreationDate());
+
+
+			if(pstmt.executeUpdate() == 1) {
+				isSucc = true;
+			}
+
+		} catch (SQLException sqle) {
+			System.out.println("INSERT문에서 예외 발생");
+			sqle.printStackTrace();
+
+		} finally {
+			if (conn != null)
+				DBCP.returnConnection(conn);
+
+		}
+
+		return isSucc;
 	}
 
 }
--- Server/model/dao/UnivDAO.java
+++ Server/model/dao/UnivDAO.java
@@ -55,46 +55,46 @@ public class UnivDAO {
 
         return dto;
     }
+//
+//    public String[][] getUnivList() {  // 학교 목록 반환하는 메소드
+//
+//        String[][] univList = null;     // 학교 목록 선언 '학교id, 학교이름'의 2차원 배열
+//
+//        String SQL = "SELECT * FROM crtvp.\"univ\"";
+//
+//        Connection conn = DBCP.getConnection();
+//
+//        try(Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+//            ResultSet rs = stmt.executeQuery(SQL);
+//        ) {
+//
+//            rs.last();                      // 행 개수 세기 위해 결과셋의 마지막 행으로 이동
+//            int rowCount = rs.getRow();
+//            rs.beforeFirst();               // 처음 행으로 이동
+//
+//            univList = new String[rowCount][2];
+//
+//            int i = 0;
+//            while (rs.next()) {
+//                univList[i][0] = rs.getString("univ_id");
+//                univList[i][1] = rs.getString("univ_name");
+//                i++;
+//            }
+//
+//        } catch (SQLException sqle) {
+//            System.out.println("Exception : SELECT");
+//            sqle.printStackTrace();
+//
+//        } finally {
+//            if (conn != null)
+//                DBCP.returnConnection(conn);
+//
+//        }
+//
+//        return univList;
+//    }
 
-    public String[][] getUnivList2() throws Exception {  // 학교 목록 반환하는 메소드
-
-        String[][] univList = null;     // 학교 목록 선언 '학교id, 학교이름'의 2차원 배열
-
-        String SQL = "SELECT * FROM crtvp.\"univ\"";
-
-        Connection conn = DBCP.getConnection();
-
-        try(Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-            ResultSet rs = stmt.executeQuery(SQL);
-        ) {
-
-            rs.last();                      // 행 개수 세기 위해 결과셋의 마지막 행으로 이동
-            int rowCount = rs.getRow();
-            rs.beforeFirst();               // 처음 행으로 이동
-
-            univList = new String[rowCount][2];
-
-            int i = 0;
-            while (rs.next()) {
-                univList[i][0] = rs.getString("univ_id");
-                univList[i][1] = rs.getString("univ_name");
-                i++;
-            }
-
-        } catch (SQLException sqle) {
-            System.out.println("Exception : SELECT");
-            sqle.printStackTrace();
-
-        } finally {
-            if (conn != null)
-                DBCP.returnConnection(conn);
-
-        }
-
-        return univList;
-    }
-
-    public HashMap<String, String> getUnivList() throws Exception {  // 학교 목록 반환하는 메소드
+    public HashMap<String, String> getUnivList() {  // 학교 목록 반환하는 메소드
 
         // Key = 학교 이름,  Value = 학교 ID
         HashMap<String, String> univList = null;     // 학교 목록 선언 '학교id, 학교이름'의 HashMap
